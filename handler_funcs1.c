c4957786cd64cec3203b916e81d5709137d80455
edwinogwel <edogwel@gmail.com>
2022-06-16 15:52:38 +0000

    Fix betty errors

--- handler_funcs1.c
+++ handler_funcs1.c
@@ -8,16 +8,16 @@
  */
 void pint_handler(stack_t **stack, unsigned int line_number)
 {
-        stack_t *head = *stack;
+	stack_t *head = *stack;
 
-        if (!head)
-        {
-                dprintf(STDERR_FILENO, PINT_FAIL, line_number);
-                free_all(1);
-                exit(EXIT_FAILURE);
-        }
+	if (!head)
+	{
+		dprintf(STDERR_FILENO, PINT_FAIL, line_number);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
 
-        printf("%d\n", head->n);
+	printf("%d\n", head->n);
 }
 
 /**
@@ -27,16 +27,16 @@ void pint_handler(stack_t **stack, unsigned int line_number)
  */
 void pop_handler(stack_t **stack, unsigned int line_number)
 {
-        stack_t *temp = *stack;
+	stack_t *temp = *stack;
 
-        if (!temp)
-        {
-                dprintf(STDERR_FILENO, POP_FAIL, line_number);
-                free_all(1);
-                exit(EXIT_FAILURE);
-        }
+	if (!temp)
+	{
+		dprintf(STDERR_FILENO, POP_FAIL, line_number);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
 
-        delete_dnodeint_at_index(stack, 0);
+	delete_dnodeint_at_index(stack, 0);
 }
 
 /**
@@ -46,26 +46,26 @@ void pop_handler(stack_t **stack, unsigned int line_number)
  */
 void swap_handler(stack_t **stack, unsigned int line_number)
 {
-        stack_t *temp = *stack, *node = NULL;
-        int num;
+	stack_t *temp = *stack, *node = NULL;
+	int num;
 
-        if (dlistint_len(*stack) < 2)
-        {
-                dprintf(STDERR_FILENO, SWAP_FAIL, line_number);
-                free_all(1);
-                exit(EXIT_FAILURE);
-        }
+	if (dlistint_len(*stack) < 2)
+	{
+		dprintf(STDERR_FILENO, SWAP_FAIL, line_number);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
 
-        temp = get_dnodeint_at_index(*stack, 0);
-        num = temp->n;
-        delete_dnodeint_at_index(stack, 0);
-        node = insert_dnodeint_at_index(stack, 1, num);
-        if (!node)
-        {
-                dprintf(STDERR_FILENO, MALLOC_FAIL);
-                free_all(1);
-                exit(EXIT_FAILURE);
-        }
+	temp = get_dnodeint_at_index(*stack, 0);
+	num = temp->n;
+	delete_dnodeint_at_index(stack, 0);
+	node = insert_dnodeint_at_index(stack, 1, num);
+	if (!node)
+	{
+		dprintf(STDERR_FILENO, MALLOC_FAIL);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
 }
 
 /**
@@ -75,28 +75,28 @@ void swap_handler(stack_t **stack, unsigned int line_number)
  */
 void add_handler(stack_t **stack, unsigned int line_number)
 {
-        int sum = 0;
-        stack_t *node = NULL;
-        stack_t *node_0 = get_dnodeint_at_index(*stack, 0);
-        stack_t *node_1 = get_dnodeint_at_index(*stack, 1);
+	int sum = 0;
+	stack_t *node = NULL;
+	stack_t *node_0 = get_dnodeint_at_index(*stack, 0);
+	stack_t *node_1 = get_dnodeint_at_index(*stack, 1);
 
-        if (dlistint_len(*stack) < 2)
-        {
-                dprintf(STDERR_FILENO, ADD_FAIL, line_number);
-                free_all(1);
-                exit(EXIT_FAILURE);
-        }
+	if (dlistint_len(*stack) < 2)
+	{
+		dprintf(STDERR_FILENO, ADD_FAIL, line_number);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
 
-        sum = node_0->n + node_1->n;
-        delete_dnodeint_at_index(stack, 0);
-        delete_dnodeint_at_index(stack, 0);
-        node = add_dnodeint(stack, sum);
-        if (!node)
-        {
-                dprintf(STDERR_FILENO, MALLOC_FAIL);
-                free_all(1);
-                exit(EXIT_FAILURE);
-        }
+	sum = node_0->n + node_1->n;
+	delete_dnodeint_at_index(stack, 0);
+	delete_dnodeint_at_index(stack, 0);
+	node = add_dnodeint(stack, sum);
+	if (!node)
+	{
+		dprintf(STDERR_FILENO, MALLOC_FAIL);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
 }
 
 /**
@@ -106,6 +106,6 @@ void add_handler(stack_t **stack, unsigned int line_number)
  */
 void nop_handler(stack_t **stack, unsigned int line_number)
 {
-        (void)stack;
-        (void)line_number;
+	(void)stack;
+	(void)line_number;
 }
